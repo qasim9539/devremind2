a538d32d3bdaa6df871b87569124500f6fd7495f
QASIM <qasimgardezi04@gmail.com>
2024-12-16 05:10:20 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const errorMiddleware = require('./middlewares/error-middleware');
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://devremind2.vercel.app',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
 };
